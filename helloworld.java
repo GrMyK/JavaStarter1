0514b165721abe6cc4e7404caf381a9b6a6124c5
GrMyk <kulemetos@gmail.com>
2019-05-24 16:04:43 +0300

    local replace

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+package com.example.helloworld;
+/** try to**/
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");// display the string.
+    }
+    }
